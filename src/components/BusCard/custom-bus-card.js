dc2d6f7ea940eacb8a3d13445e9c9af9925be6e9
1812 <francogadea1812@gmail.com>
2025-02-27 15:48:05 -0300

    Trabajando en el componente CardBus

--- src/components/BusCard/custom-bus-card.js
+++ src/components/BusCard/custom-bus-card.js
@@ -1,7 +1,9 @@
-import BusCard_css from './custom-bus-card.css?url';
 import bookmark_icon from "bootstrap-icons/icons/bookmark.svg?raw";
 import bookmark_fill_icon from "bootstrap-icons/icons/bookmark-fill.svg?raw";
-import bootstrap_css from "bootstrap/dist/css/bootstrap.min.css?url";
+import stops_icon from "bootstrap-icons/icons/geo-alt.svg?raw";
+import report_icon from "bootstrap-icons/icons/exclamation-triangle.svg?raw";
+
+import SAVED_BUS_PATHS from '@/scripts/localStorage/SavedBusPaths.js';
 
 export class CustomCardBus extends HTMLElement {
 
@@ -10,11 +12,7 @@ export class CustomCardBus extends HTMLElement {
 
         this.src = this.getAttribute('src');
 
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = `
-            <link rel="stylesheet" href="${BusCard_css}">
-            <link rel="stylesheet" href="${bootstrap_css}">
-
+        this.innerHTML = `
             <div class="Bus-content" role="button">
                 <img class="Bus-image" />
 
@@ -23,19 +21,27 @@ export class CustomCardBus extends HTMLElement {
                     <p class="Bus-direction"></p>
 
                     <div class="Bus-footer">
-                        <span class="Bus-path-length"></span>
                         <span class="Bus-provider"></span>
+                        <span class="Bus-path-length"></span>
                     </div>
                 </div>
             </div>
             
             <div class="Bus-controls">
-                <button class="Save-btn btn">
+                <button class="Save-btn btn" title="Guardar">
                     <div class="save-icon">${bookmark_icon}</div>
                     <div class="saved-icon">${bookmark_fill_icon}</div>
                 </button>
+                <button class="Stops-btn btn" title="Mostrar paradas">
+                    <div class="stops-icon">${stops_icon}</div>
+                </button>
+                <button class="Report-btn btn" title="Corregir recorrido">
+                    <div class="report-icon">${report_icon}</div>
+                </button>
             </div>
         `;
+
+        this.$ = (selector) => this.querySelector(selector);
         
         this.init();
     }
@@ -47,45 +53,57 @@ export class CustomCardBus extends HTMLElement {
 
         this.data = data;
 
-        this.shadowRoot.querySelector('.Bus-image').src = data.image;
-        this.shadowRoot.querySelector('.Bus-title').textContent = data.name;
+        this.$('.Bus-image').src = data.image.square;
+        this.$('.Bus-title').textContent = data.name;
 
-        this.shadowRoot.querySelector('.Bus-direction').textContent = data.direction;
-        this.shadowRoot.querySelector('.Bus-path-length').textContent = data.length_km + ' km';
-        this.shadowRoot.querySelector('.Bus-provider').textContent = data.provider.name;
+        this.$('.Bus-direction').textContent = data.direction;
+        this.$('.Bus-path-length').textContent = data.length_km + ' km';
+        this.$('.Bus-provider').textContent = data.provider.name;
 
-        this.saved = JSON.parse(localStorage.getItem('busPaths'))?.includes(data.id);
+        //Save Bus Path
+        this.initSaveBtn();
 
-        this.saved && this.setAttribute('saved', '');
-
-        this.shadowRoot.querySelector('.Save-btn').addEventListener('click', () => this.saveBusPath());
+        this.initMainButton();
     }
 
     saved = false;
+
+    initSaveBtn(){
+
+        this.saved = SAVED_BUS_PATHS.has(this.data.id);
+        this.saved && this.setAttribute('saved', '');
+
+        SAVED_BUS_PATHS.on((busPaths) => {
+
+            this.saved = busPaths.includes(this.data.id);
+            this.saved ? this.setAttribute('saved', '') : this.removeAttribute('saved');
+        });
+
+        this.$('.Save-btn').addEventListener('click', () => {
+
+            if(!this.saved){
+
+                SAVED_BUS_PATHS.add(this.data.id);
+                this.setAttribute('saved', ''); 
+            }
+            else {
     
-    saveBusPath(){
+                SAVED_BUS_PATHS.remove(this.data.id);
+                this.removeAttribute('saved'); 
+            }
+    
+            this.saved = SAVED_BUS_PATHS.has(this.data.id);
+        });
+    }
 
-        //Init Local Storage
-        const savedBuspaths = (() => {
+    active = false;
 
-            const existing = localStorage.getItem('busPaths');
+    initMainButton(){
 
-            if(!existing) localStorage.setItem('busPaths', JSON.stringify([]));
+        this.$('.Bus-content').addEventListener('click', () => {
 
-            return existing ? JSON.parse(existing) : [];
-        })();
-
-        if(!this.saved){
-
-            localStorage.setItem('busPaths', JSON.stringify([...savedBuspaths, this.data.id]));
-            this.setAttribute('saved', ''); 
-        }
-        else {
-
-            localStorage.setItem('busPaths', JSON.stringify(savedBuspaths.filter(id => id !== this.data.id)));
-            this.removeAttribute('saved'); 
-        }
-
-        this.saved = !this.saved;
+            this.active = !this.active;
+            this.active ? this.setAttribute('active', '') : this.removeAttribute('active');
+        });
     }
 }
--- src/scripts/localStorage/SavedBusPaths.js
+++ src/scripts/localStorage/SavedBusPaths.js
@@ -2,16 +2,75 @@
 
 export class SavedBusPaths {
 
+    static NAME = 'busPaths';
+
+    #listeners = new Set();
+
     constructor() {
 
         //Init Local Storage
-        if (localStorage.getItem('busPaths') === null) {
+        if (localStorage.getItem(SavedBusPaths.NAME) === null) {
 
-            localStorage.setItem('busPaths', JSON.stringify([]));
+            localStorage.setItem(SavedBusPaths.NAME, '[]');
         }
 
         this.busPaths = JSON.parse(localStorage.getItem('busPaths'));
+
+        window.addEventListener('storage', (e) => {
+
+            if (e.key === SavedBusPaths.NAME) {
+
+                this.busPaths = JSON.parse(e.newValue);
+
+                this.#listeners.forEach((callback) => callback(this.busPaths));
+            }
+        });
     }
 
-    
+    on(callback) {
+
+        this.#listeners.add(callback);
+    }
+
+    off(callback) {
+
+        this.#listeners.delete(callback);
+    }
+
+    offAll() {
+            
+        this.#listeners.clear();
+    }
+
+    async add(busPath) {
+
+        this.busPaths.push(busPath);
+
+        localStorage.setItem(SavedBusPaths.NAME, JSON.stringify(this.busPaths));
+
+        this.#listeners.forEach((callback) => callback(this.busPaths));
+    }
+
+    async remove(busPath) {
+            
+        this.busPaths = this.busPaths.filter((path) => path !== busPath);
+        
+        localStorage.setItem(SavedBusPaths.NAME, JSON.stringify(this.busPaths));
+
+        this.#listeners.forEach((callback) => callback(this.busPaths));
+    }
+
+    has(busPath) {
+
+        return this.busPaths.includes(busPath);
+    }
+
+    clear() {
+
+        localStorage.setItem(SavedBusPaths.NAME, '[]');
+    }
 }
+
+const SAVED_BUS_PATHS = new SavedBusPaths();
+
+export default SAVED_BUS_PATHS;
